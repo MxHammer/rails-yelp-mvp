f897b41c7fa7137be38b40adfa1373fbc8cd83c9
Max <mxhmmrstn@gmail.com>
2022-11-16 19:50:24 +0100

    Second middle

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
-  validates :ratings, :content, presence: true
+  validates :content, :rating, presence: true
+  validates :rating, numericality: { only_integer: true }
+  validates :rating, inclusion: { in: (0..5) }
 end
